eda73f1c371b5be3aa793b52c7b638bf6398d193
Ulf Portner <ulf.portner@googlemail.com>
2011-10-17 15:44:12 +0200

    Added some asset-configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,7 @@ Subscriber::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = true  
 end
+
 
